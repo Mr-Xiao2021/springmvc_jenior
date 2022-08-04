2f42e863d7c04495ca86e728c248b30a2b16eda2
Mr-Xiao2021 <3493602396@qq.com>
2022-08-04 09:38:36 +0800

    first-commit

---
+++ src/TestHello.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName TestHello
+ * @Description TODO
+ * @Author Mr_X
+ * @Date 2022/8/4 9:37
+ * @Version 1.0
+ */
+public class TestHello {
+    public static void main(String[] args) {
+        System.out.println("hello springmvc");
+    }
+}
